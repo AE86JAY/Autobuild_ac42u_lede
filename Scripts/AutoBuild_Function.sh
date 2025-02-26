b27e552d4ac933c56e71c981a45498828cc985c8
AE86JAY <AE86JAY@users.noreply.github.com>
2025-02-26 21:54:17 +0800

    更新 AutoBuild_Function.sh

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -75,7 +75,7 @@ Firmware_Diy_Start() {
 	if [[ ${Default_Flag} == AUTO ]]
 	then
 		TARGET_FLAG=${CONFIG_FILE/${TARGET_PROFILE}-/}
-		[[ ${TARGET_FLAG} =~ ${TARGET_PROFILE} || -z ${TARGET_FLAG} || ${TARGET_FLAG} == ${CONFIG_FILE} ]] && TARGET_FLAG=${OP_REPO}
+		[[ ${TARGET_FLAG} =~ ${TARGET_PROFILE} || -z ${TARGET_FLAG} || ${TARGET_FLAG} == ${CONFIG_FILE} ]] && TARGET_FLAG=full
 	else
 		if [[ ! ${Default_Flag} =~ (\"|=|-|_|\.|\#|\|) && ${Default_Flag} =~ [a-zA-Z0-9] ]]
 		then
